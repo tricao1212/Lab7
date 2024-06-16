2325ba0c8dd7074d5f1ae240d45ef12cca64a246
tricao1212 <108685746+tricao1212@users.noreply.github.com>
2024-06-17 01:28:40 +0700

    update

--- Lab7/src/screens/AddTransaction.js
+++ Lab7/src/screens/AddTransaction.js
@@ -6,15 +6,135 @@ import {
   StyleSheet,
   FlatList,
   TouchableOpacity,
+  Alert,
 } from 'react-native';
-import React, {useCallback, useEffect, useRef, useState} from 'react';
+import React, {
+  createContext,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import {Dropdown} from 'react-native-element-dropdown';
 import axios from 'axios';
 import Loading from '../components/Loading';
 import BouncyCheckbox from 'react-native-bouncy-checkbox';
-import {Button} from 'react-native-paper';
-import {useFocusEffect} from '@react-navigation/native';
 import {useSelector} from 'react-redux';
+const PriceContext = createContext();
+
+const ServiceBlock = React.memo(({setTotalPrice, data, list}) => {
+  const user = useSelector(state => state.UserInfo.user);
+  const [quan, setQuan] = useState(1);
+  const [isChecked, setIsChecked] = useState(false);
+  const [price, setPrice] = useState(data.price);
+  let tempPrice = data.price;
+  let tempQuan = 1;
+
+  const updateQuanPrice = useCallback((serviceId, quantity, price) => {
+    const data = {
+      id: serviceId,
+      quantity: quantity,
+      price: price,
+    };
+    const index = list.findIndex(ser => ser.id === serviceId);
+    list[index] = data;
+    console.log(list);
+    let temp = 0;
+    for (let obj of list) {
+      if (isNaN(obj.price) == false) {
+        temp += obj.price;
+      }
+      setTotalPrice(temp);
+    }
+  }, []);
+
+  const updateSelectService = useCallback(
+    (serviceId, quantity, price, isChecked) => {
+      const data = {
+        id: serviceId,
+        quantity: quantity,
+        price: price,
+      };
+      if (isChecked) {
+        const index = list.findIndex(ser => ser.id === serviceId);
+        index !== -1 ? {} : list.push(data);
+      } else {
+        const index = list.findIndex(ser => ser.id === serviceId);
+        index !== -1 ? list.splice(index, 1) : {};
+      }
+      console.log(list);
+      let temp = 0;
+      for (let obj of list) {
+        if (isNaN(obj.price) == false) {
+          temp += obj.price;
+        }
+        setTotalPrice(temp);
+      }
+    },
+    [],
+  );
+
+  const increase = useCallback(() => {
+    setQuan(prevQuan => prevQuan + 1);
+    setPrice(prevPrice => (prevPrice += data.price));
+    tempPrice += data.price;
+    tempQuan++;
+    updateQuanPrice(data._id, tempQuan, tempPrice);
+  }, [quan]);
+
+  const decrease = useCallback(() => {
+    setQuan(prevQuan => (prevQuan > 1 ? prevQuan - 1 : 1));
+    setPrice(prevPrice =>
+      prevPrice > price ? (prevPrice -= data.price) : data.price,
+    );
+    tempPrice > price ? (tempPrice -= data.price) : data.price;
+    tempQuan > 1 ? tempQuan-- : 1;
+    updateQuanPrice(data._id, tempQuan, tempPrice);
+  }, [quan]);
+
+  const check = useCallback(() => {
+    setIsChecked(prevIsChecked => {
+      const newIsChecked = !prevIsChecked;
+      updateSelectService(data._id, tempQuan, tempPrice, newIsChecked);
+      return newIsChecked;
+    });
+  }, [isChecked]);
+
+  return (
+    <View style={styles.block}>
+      <BouncyCheckbox
+        isChecked={isChecked}
+        onPress={() => check()}
+        text={data.name}
+        textStyle={
+          isChecked ? styles.checkedTextStyle : styles.uncheckedTextStyle
+        }
+      />
+      {isChecked && (
+        <View>
+          <View style={styles.additionalComponent}>
+            <View style={styles.left}>
+              <TouchableOpacity style={styles.btn} onPress={() => decrease()}>
+                <Text>-</Text>
+              </TouchableOpacity>
+              <Text style={styles.quantityText}>{quan}</Text>
+              <TouchableOpacity style={styles.btn} onPress={() => increase()}>
+                <Text>+</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.right}>
+              <Text>Executor: {user.name}</Text>
+            </View>
+          </View>
+          <View style={styles.priceSer}>
+            <Text>Price: {price}</Text>
+          </View>
+        </View>
+      )}
+    </View>
+  );
+});
+
 const AddTransaction = () => {
   const user = useSelector(state => state.UserInfo.user);
   const [customer, setCustomer] = useState();
@@ -23,15 +143,39 @@ const AddTransaction = () => {
   const [value, setValue] = useState(null);
   const [isFocus, setIsFocus] = useState(false);
   const [totalPrice, setTotalPrice] = useState(0);
-  const selectedServices = useRef([]);
-  console.log('render');
-  //   const handleCreateTrans = async () =>{
-  //     const postData = {
-  //         customerId :
-  //     };
-
-  //     await axios.post('https://kami-backend-5rs0.onrender.com/transactions')
-  //   }
+  let selectedServices = [];
+  const handleCreateTrans = async () => {
+    const postData = {
+      customerId: customerId,
+      services: selectedServices?.map(s => ({
+        _id: s.id,
+        quantity: s.quantity,
+        userId: user._id,
+      })),
+    };
+    !customerId
+      ? Alert.alert('Choose customer please!')
+      : await axios
+          .post(
+            'https://kami-backend-5rs0.onrender.com/transactions',
+            postData,
+            {
+              headers: {
+                Authorization: `Bearer ${user.token}`,
+              },
+            },
+          )
+          .then(res => {
+            if (res.status == 200) {
+              Alert.alert('Add transaction successfully');
+            } else {
+              Alert.alert('Something wrong !');
+            }
+          })
+          .catch(error => {
+            console.log(error);
+          });
+  };
   const fectCustomer = async () => {
     await axios
       .get('https://kami-backend-5rs0.onrender.com/customers')
@@ -59,138 +203,57 @@ const AddTransaction = () => {
     fectService();
   }, []);
 
-  const updateSelectService = useCallback((serviceId, quantity, price, isChecked) => {
-    const data = {
-      id: serviceId,
-      quantity: quantity,
-      price: price,
-    };
-    if (isChecked) {
-      const index = selectedServices.current.findIndex(ser => ser.id === serviceId);
-      index !== -1 ? {} : selectedServices.current.push(data);
-      console.log(selectedServices);
-    } else {
-      const index = selectedServices.current.findIndex(ser => ser.id === serviceId);
-      index !== -1 ? selectedServices.current.splice(index, 1) : {};
-    }
-
-    let temp = 0;
-    for (let obj of selectedServices.current) {
-      if (isNaN(obj.price) == false) {
-        temp += obj.price;
-      }
-    }
-    setTotalPrice(temp);
-  },[]);
-
-  const ServiceBlock = React.memo(data => {
-    const [quan, setQuan] = useState(1);
-    const [isChecked, setIsChecked] = useState(false);
-    const [price, setPrice] = useState(data.data.price);
-    let tempPrice = data.data.price;
-    let tempQuan = 1;
-    const increase = useCallback(() => {
-      setQuan(prevQuan => prevQuan + 1);
-      setPrice(prevPrice => (prevPrice += data.data.price));
-      tempPrice += data.data.price;
-      tempQuan++;
-    }, []);
-
-    const decrease = useCallback(() => {
-      setQuan(prevQuan => (prevQuan > 1 ? prevQuan - 1 : 1));
-      setPrice(prevPrice =>
-        prevPrice > price ? (prevPrice -= data.data.price) : data.data.price,
-      );
-      tempPrice > price ? (tempPrice -= data.data.price) : data.data.price;
-      tempQuan > 1 ? tempQuan-- : 1;
-    }, []);
-
-    const check = useCallback(() => {
-      setIsChecked(prevIsChecked => {
-        const newIsChecked = !prevIsChecked;
-        updateSelectService(data.data._id, tempQuan, tempPrice, newIsChecked);
-        return newIsChecked;
-      });
-    }, []);
-
-    return (
-      <View style={styles.block}>
-        <BouncyCheckbox
-          isChecked={isChecked}
-          onPress={() => check()}
-          text={data.data.name}
-          textStyle={
-            isChecked ? styles.checkedTextStyle : styles.uncheckedTextStyle
-          }
-        />
-        {isChecked && (
-          <View>
-            <View style={styles.additionalComponent}>
-              <View style={styles.left}>
-                <TouchableOpacity style={styles.btn} onPress={() => decrease()}>
-                  <Text>-</Text>
-                </TouchableOpacity>
-                <Text style={styles.quantityText}>{quan}</Text>
-                <TouchableOpacity style={styles.btn} onPress={() => increase()}>
-                  <Text>+</Text>
-                </TouchableOpacity>
-              </View>
-              <View style={styles.right}>
-                <Text>Executor: {user.name}</Text>
-              </View>
-            </View>
-            <View style={styles.priceSer}>
-              <Text>Price: {price}</Text>
-            </View>
-          </View>
-        )}
-      </View>
-    );
-  });
-
   if (!customer && !service) {
     return <Loading />;
   }
   return (
-    <SafeAreaView style={styles.main}>
-      <Text style={styles.custext}>Customer*</Text>
-      <Dropdown
-        style={[styles.dropdown, isFocus && {borderColor: 'blue'}]}
-        placeholderStyle={styles.placeholderStyle}
-        selectedTextStyle={styles.selectedTextStyle}
-        inputSearchStyle={styles.inputSearchStyle}
-        iconStyle={styles.iconStyle}
-        data={customer}
-        search
-        maxHeight={300}
-        labelField="name"
-        valueField="name"
-        placeholder={!isFocus ? 'Select item' : '...'}
-        searchPlaceholder="Search..."
-        value={value}
-        onFocus={() => setIsFocus(true)}
-        onBlur={() => setIsFocus(false)}
-        onChange={item => {
-          setCustomerId(item._id);
-          setValue(item.name);
-          setIsFocus(false);
-        }}
-      />
-      <View style={styles.list}>
-        <FlatList
-          data={service}
-          renderItem={({item}) => <ServiceBlock data={item} />}
-          keyExtractor={item => item._id}
+    <PriceContext.Provider value={totalPrice}>
+      <SafeAreaView style={styles.main}>
+        <Text style={styles.custext}>Customer*</Text>
+        <Dropdown
+          style={[styles.dropdown, isFocus && {borderColor: 'blue'}]}
+          placeholderStyle={styles.placeholderStyle}
+          selectedTextStyle={styles.selectedTextStyle}
+          inputSearchStyle={styles.inputSearchStyle}
+          iconStyle={styles.iconStyle}
+          data={customer}
+          search
+          maxHeight={300}
+          labelField="name"
+          valueField="name"
+          placeholder={!isFocus ? 'Select item' : '...'}
+          searchPlaceholder="Search..."
+          value={value}
+          onFocus={() => setIsFocus(true)}
+          onBlur={() => setIsFocus(false)}
+          onChange={item => {
+            setCustomerId(item._id);
+            setValue(item.name);
+            setIsFocus(false);
+          }}
         />
-      </View>
-      <View style={styles.bottomButtonContainer}>
-        <TouchableOpacity style={styles.summary}>
-          <Text style={styles.summaryText}>
-            See summary: {totalPrice}đ
-          </Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
+        <View style={styles.list}>
+          <FlatList
+            data={service}
+            renderItem={({item}) => (
+              <ServiceBlock
+                data={item}
+                setTotalPrice={setTotalPrice}
+                list={selectedServices}
+              />
+            )}
+            keyExtractor={item => item._id}
+          />
+        </View>
+        <View style={styles.bottomButtonContainer}>
+          <TouchableOpacity
+            style={styles.summary}
+            onPress={() => handleCreateTrans()}>
+            <Text style={styles.summaryText}>See summary: {totalPrice}đ</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </PriceContext.Provider>
   );
 };
 
